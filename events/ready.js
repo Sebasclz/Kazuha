cf973462c99a8bf0e737710bdf562e5ef44f3397
Eduardo Sebastian Tejeda <eduperez392@gmail.com>
2021-10-30 13:37:39 -0300

    minor changes console.log

--- events/ready.js
+++ events/ready.js
@@ -8,6 +8,6 @@ module.exports = {
 			useNewUrlParser: true,
 			useUnifiedTopology: true, 
 		})
-		console.log('Shard iniciada')
+		console.log('Base de datos y bot iniciados correctamente.')
 	}
 }
