89e430e92f1dff13458a5fec24ecf8d5ef3daa0e
Eduardo Sebastian Tejeda <eduperez392@gmail.com>
2021-11-01 12:54:02 -0300

    bugfixes

--- commands/Informacion/covid.js
+++ commands/Informacion/covid.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 const config = require('../../config.json')
-const fetch = require('node-fetch')
+const superagent = require('superagent')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,41 +15,85 @@ module.exports = {
             try{
                 const pais = interaction.options.getString('pais')
 
-                const res = await fetch(`https://corona.lmao.ninja/v2/countries/${pais}`)
-                const json = await res.json()
+                const paisString = pais.toString()
 
-                const embedError = new MessageEmbed()
-                .setTitle('Error')
-                .setDescription('El nombre del pais es invalido')
-                .setFooter('Los nombres de los paises NO LATINOS se escriben en INGLES', interaction.user.avatarURL({ dynamic: true }))
-                .setTimestamp()
-                .setColor(config.defaultErrorColor)
+                if(paisString === 'España' || 'españa'){
+                    superagent
+                .get(`https://disease.sh/v3/covid-19/countries/spain`)
+                .end((err, res) => {
+                  const body = res.body
 
-                if(json.message) return interaction.reply({ embeds: [embedError]})
+                    const embedError = new MessageEmbed()
+                    .setTitle('Error')
+                    .setDescription('El nombre del pais es invalido')
+                    .setFooter('Los nombres de los paises NO LATINOS se escriben en INGLES', interaction.user.avatarURL({ dynamic: true }))
+                    .setTimestamp()
+                    .setColor(config.defaultErrorColor)
+    
+                    if(body.message) return interaction.reply({ embeds: [embedError]})
 
-                const embed = new MessageEmbed()
-                .setTitle(`Informacion del covid en ${json.country}`)
-                .setThumbnail(`${json.countryInfo.flag}`)
-                .addField('Casos totales', `${json.cases}`)
-                .addField('Casos totales por cada millon de habitantes', `${json.casesPerOneMillion}`)
-                .addField('Casos hoy', `${json.todayCases}`)
-                .addField('Casos activos por cada millon de habitantes', `${json.activePerOneMillion}`)
-                .addField('Casos criticos', `${json.critical}`)
-                .addField('Casos criticos por cada millon de habitantes', `${json.criticalPerOneMillion}`)
-                .addField('Muertes totales', `${json.deaths}`)
-                .addField('Muertes totales por cada millon de habitantes', `${json.deathsPerOneMillion}`)
-                .addField('Muertes hoy', `${json.todayDeaths}`)
-                .addField('Recuperados', `${json.recovered}`)
-                .addField('Recuperados hoy', `${json.todayRecovered}`)
-                .addField('Casos recuperados por cada millon de habitantes', `${json.recoveredPerOneMillion}`)
-                .addField('Tests hechos', `${json.tests}`)
-                .addField('Tests por cada millon de habitantes', `${json.testsPerOneMillion}`)
-                .setColor(config.defaultSuccessColor)
-                .setFooter(`Covid-19 ${json.country}`, interaction.user.avatarURL({ dynamic: true }))
-                .setTimestamp()
+                    const embed = new MessageEmbed()
+                    .setTitle(`Informacion del covid en ${body.country}`)
+                    .setThumbnail(`${body.countryInfo.flag}`)
+                    .addField('Casos totales', `${body.cases}`)
+                    .addField('Casos totales por cada millon de habitantes', `${body.casesPerOneMillion}`)
+                    .addField('Casos hoy', `${body.todayCases}`)
+                    .addField('Casos activos por cada millon de habitantes', `${body.activePerOneMillion}`)
+                    .addField('Casos criticos', `${body.critical}`)
+                    .addField('Casos criticos por cada millon de habitantes', `${body.criticalPerOneMillion}`)
+                    .addField('Muertes totales', `${body.deaths}`)
+                    .addField('Muertes totales por cada millon de habitantes', `${body.deathsPerOneMillion}`)
+                    .addField('Muertes hoy', `${body.todayDeaths}`)
+                    .addField('Recuperados', `${body.recovered}`)
+                    .addField('Recuperados hoy', `${body.todayRecovered}`)
+                    .addField('Casos recuperados por cada millon de habitantes', `${body.recoveredPerOneMillion}`)
+                    .addField('Tests hechos', `${body.tests}`)
+                    .addField('Tests por cada millon de habitantes', `${body.testsPerOneMillion}`)
+                    .setColor(config.defaultSuccessColor)
+                    .setFooter(`Covid-19 ${body.country}`, interaction.user.avatarURL({ dynamic: true }))
+                    .setTimestamp()
+    
+                    return interaction.reply({ embeds: [embed]})
+                })
+                } else {
+                superagent
+                .get(`https://disease.sh/v3/covid-19/countries/${pais}`)
+                .end((err, res) => {
+                  const body = res.body
 
-                return interaction.reply({ embeds: [embed]})
+                    const embedError = new MessageEmbed()
+                    .setTitle('Error')
+                    .setDescription('El nombre del pais es invalido')
+                    .setFooter('Los nombres de los paises NO LATINOS se escriben en INGLES', interaction.user.avatarURL({ dynamic: true }))
+                    .setTimestamp()
+                    .setColor(config.defaultErrorColor)
+    
+                    if(body.message) return interaction.reply({ embeds: [embedError]})
 
+                    const embed = new MessageEmbed()
+                    .setTitle(`Informacion del covid en ${body.country}`)
+                    .setThumbnail(`${body.countryInfo.flag}`)
+                    .addField('Casos totales', `${body.cases}`)
+                    .addField('Casos totales por cada millon de habitantes', `${body.casesPerOneMillion}`)
+                    .addField('Casos hoy', `${body.todayCases}`)
+                    .addField('Casos activos por cada millon de habitantes', `${body.activePerOneMillion}`)
+                    .addField('Casos criticos', `${body.critical}`)
+                    .addField('Casos criticos por cada millon de habitantes', `${body.criticalPerOneMillion}`)
+                    .addField('Muertes totales', `${body.deaths}`)
+                    .addField('Muertes totales por cada millon de habitantes', `${body.deathsPerOneMillion}`)
+                    .addField('Muertes hoy', `${body.todayDeaths}`)
+                    .addField('Recuperados', `${body.recovered}`)
+                    .addField('Recuperados hoy', `${body.todayRecovered}`)
+                    .addField('Casos recuperados por cada millon de habitantes', `${body.recoveredPerOneMillion}`)
+                    .addField('Tests hechos', `${body.tests}`)
+                    .addField('Tests por cada millon de habitantes', `${body.testsPerOneMillion}`)
+                    .setColor(config.defaultSuccessColor)
+                    .setFooter(`Covid-19 ${body.country}`, interaction.user.avatarURL({ dynamic: true }))
+                    .setTimestamp()
+    
+                    return interaction.reply({ embeds: [embed]})
+                })
+            }
             } catch (e){ //Si da error le avisamos al usuario y lo reportamos al servidor
                 console.error(e)
                 interaction.reply({ embeds: [
